3b1bb8e1e3e8c93f6195420372f25d1d6b652a1c
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-18 20:35:26 -0400

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Signin' element={<Signin/>}/>
+        <Route path='/Signup' element={<Signup/>}/>
+        <Route path='/Profile' element={<Profile/>}/>
+        <Route path='/About' element={<About/>}/>
+
+      </Routes>
+      </BrowserRouter>
+    );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  <h1>HELOOO!!!!</h1>
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      <p className="mt-2">This is a basic example of using Tailwind CSS classes in a React component.</p>
+    </div>
+  );
+};
+
