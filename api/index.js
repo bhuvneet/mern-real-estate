8a84478d3f6213c464281b535816f598a1627c52
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-19 19:50:16 -0400

    Added and connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import { error } from 'console';
+dotenv.config()
 
-const app = express()
+
+// connect to mongoose database
+const mongURI  = process.env.MONGO_DB
+mongoose.connect(mongURI).then(() => {
+    console.log('Connected to MDB');
+}).catch((error) => {
+    console.log(error);
+})
+
+const app = express();
 
 // listen to a port
 app.listen(3000, () =>
