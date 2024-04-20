c7b279d146a30014e84d088c36ef79e181859e64
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-19 20:33:54 -0400

    created a test API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import { error } from 'console';
+import userRouter from './routes/user.route.js'
 dotenv.config()
 
 
@@ -21,3 +22,4 @@ app.listen(3000, () =>
     console.log('Server is running on port 3000!!');
 });
 
+app.use("/api/user", userRouter)
