3b888d81209301269f99580b01d201a9a015a0b7
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-21 12:55:23 -0400

    implemented sign up page functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,20 +5,20 @@ import mongoose from "mongoose";
 
 // mongodb will check if username meets the following criteria
 const userSchema = new mongoose.Schema({
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        password: {
-            type: String,
-            required: true,
-        }
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
     }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
