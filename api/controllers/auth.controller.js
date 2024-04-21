f77a0deb31bf6439678554a756a6208c657f93d3
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-21 08:41:41 -0400

    created middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) =>
+export const signup = async (req, res, next) =>
 {
     const {username, email, password} = req.body;
     const hashedPass = bcryptjs.hashSync(password, 10)
@@ -13,7 +13,7 @@ export const signup = async (req, res) =>
     }
     catch (error)
     {
-        res.status(500).json(error.message);
+        next(error);
     }
     
     console.log(req.body)
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import { error } from 'console';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import { stat } from 'fs';
 dotenv.config()
 
 
@@ -27,3 +28,13 @@ app.listen(3000, () =>
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+
+app.use((error, req, res, next) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
