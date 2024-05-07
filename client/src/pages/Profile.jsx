27907f6109cbb06fd274d256173a5095707fe73b
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-05-07 07:41:07 -0400

    delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,7 +7,13 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice';
+import { 
+  updateUserStart, 
+  updateUserSuccess, 
+  updateUserFailure, 
+  deleteUserStart, 
+  deleteUserSuccess, 
+  deleteUserFailure } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { current } from '@reduxjs/toolkit';
 
@@ -21,10 +27,6 @@ export default function Profile() {
   const [file, setFile] = useState(undefined)
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
-
-  if (!currentUser) {
-    return <div>Please sign-in to view Profile...</div>; // or any other loading indicator
-  }
 
     useEffect(() => {
     if (file) {
@@ -91,74 +93,99 @@ export default function Profile() {
     }
   }
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+
+      dispatch(deleteUserSuccess(data))
+    }
+    catch (error) {
+      dispatch(deleteUserFailure(error.message))
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input onChange={(e)=>setFile(e.target.files[0])} 
-        type='file' ref={fileRef} hidden accept='image/*'/>
-        <img 
-          onClick={()=> fileRef.current.click()} 
-          src={formData.avatar || currentUser.avatar} 
-          alt="profile" 
-          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
-        />
+      {!currentUser && <div>Please sign-in to view Profile...</div>}
+      {currentUser && (
+      <>
+        <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          <input onChange={(e)=>setFile(e.target.files[0])} 
+          type='file' ref={fileRef} hidden accept='image/*'/>
+          <img 
+            onClick={()=> fileRef.current.click()} 
+            src={formData.avatar || currentUser.avatar} 
+            alt="profile" 
+            className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' 
+          />
 
-        <p>
-          {fileUploadError ? 
-            <span className='text-red-700'>Error uploading image. Image must be less than 2 MB</span>
-            :
-            filePerc > 0 && filePerc < 100 ? (
-              <span className='text-slate-700'>
-                {`Uploading ${filePerc}%`}
-              </span>)
+          <p>
+            {fileUploadError ? 
+              <span className='text-red-700'>Error uploading image. Image must be less than 2 MB</span>
               :
-            filePerc === 100 ? (
-              <span className='text-green-700'>Successfully uploaded</span>)
-              : (
-              ""
-          )}
-        </p>
+              filePerc > 0 && filePerc < 100 ? (
+                <span className='text-slate-700'>
+                  {`Uploading ${filePerc}%`}
+                </span>)
+                :
+              filePerc === 100 ? (
+                <span className='text-green-700'>Successfully uploaded</span>)
+                : (
+                ""
+            )}
+          </p>
 
-        <input
-          type='text'
-          placeholder='username'
-          defaultValue={currentUser.username}
-          id='username'
-          className='border p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <input
-          type='email'
-          placeholder='email'
-          id='email'
-          defaultValue={currentUser.email}
-          className='border p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='password'
-          id='password'
-          className='border p-3 rounded-lg'
-          onChange={handleChange}
-        />
+          <input
+            type='text'
+            placeholder='username'
+            defaultValue={currentUser.username}
+            id='username'
+            className='border p-3 rounded-lg'
+            onChange={handleChange}
+          />
+          <input
+            type='email'
+            placeholder='email'
+            id='email'
+            defaultValue={currentUser.email}
+            className='border p-3 rounded-lg'
+            onChange={handleChange}
+          />
+          <input
+            type='password'
+            placeholder='password'
+            id='password'
+            className='border p-3 rounded-lg'
+            onChange={handleChange}
+          />
 
-        <button
-          disabled={loading}
-          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
-        >
-          {loading ? 'Loading...' : 'Update'}
-        </button>
-      </form>
+          <button
+            disabled={loading}
+            className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+          >
+            {loading ? 'Loading...' : 'Update'}
+          </button>
+        </form>
 
-      <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
-      </div>
+        <div className='flex justify-between mt-5'>
+          <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete account</span>
+          <span className='text-red-700 cursor-pointer'>Sign out</span>
+        </div>
 
-        <p className='text-red-700 mt-5'>{error ? error : ''}</p>
-        <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
+          <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+          <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
+    </>
+    )}
     </div>
   )
 };
